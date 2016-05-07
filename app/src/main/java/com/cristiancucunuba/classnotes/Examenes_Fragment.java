2f8bae05d8524083da488c0a6aba7964cd6eb4b9
Sneider Sanchez <Sneider Sanchez>
2016-05-07 00:26:21 -0500

    Initial commit

---
+++ app/src/main/java/com/cristiancucunuba/classnotes/Examenes_Fragment.java
@@ -0,0 +1,21 @@
+package com.cristiancucunuba.classnotes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by VALENTINA on 07/05/2016.
+ */
+public class Examenes_Fragment extends Fragment{
+    View  myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.tareas,container,false);
+        return myView;
+    }
+}
